d3a4faca6306e89239e3fc9d35dbce0894bb8c83
saint6839 <saint6839@gmail.com>
2022-09-27 23:01:38 +0900

    feat: 백준 15649 solved

---
+++ src/permutationandcombination/Baekjoon15649.java
@@ -0,0 +1,45 @@
+package permutationandcombination;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon15649 {
+    private static int[] arr;
+    private static boolean[] visit;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+        dfs(N, M, 0);
+        System.out.println(sb);
+
+    }
+
+    public static void dfs(int N, int M, int depth) {
+        if (M == depth) {
+            for (int i : arr) {
+                sb.append(i)
+                  .append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(N, M, depth + 1);
+                visit[i] = false;
+            }
+        }
+    }
+}
